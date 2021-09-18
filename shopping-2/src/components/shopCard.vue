c7c451f54bfc87cad30a4ff81e6fb3512256a3a4
Nikolas <koly10111997@gmail.com>
2021-09-18 19:36:10 +0300

    Initial commit

---
+++ shopping-2/src/components/shopCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="task-card my-style">
+    <div>
+      <h4>Title:{{ model.title }}</h4>
+      <p>Cost:{{ model.cost }}$</p>
+      <p>Quantity:{{ model.quantity }}</p>
+      <p>Sum:{{ sum }}$</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.task-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+
+}
+  .task-card__buttons {
+  display: flex;
+  justify-content: space-between;
+}
+
+.task-card__checkbox {
+  margin-right:50px;
+}
+</style>
